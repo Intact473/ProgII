2de4ad547276e7cead6a6c0ce3530de5257ea279
Intact473 <patrick.stempel473@gmail.com>
2025-10-06 11:42:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Neues Projekt anlegen mit 
+dotnet new console -n MyProject
+
+dotnet build <- damit compile ich
+
+Projekt Starten 
+dotnet run
---
+++ Vorlesung/VendingMachine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+class Program
+{
+    public class VendingMachine
+    {
+        public Dictionary<string, decimal> prices = new Dictionary<string, decimal>();
+        public Dictionary<string, int> stock = new Dictionary<string, int>();
+
+        public VendingMachine()
+        {
+            prices.Add("Bier", 2.00m);
+            prices.Add("Wasser", 8.00m);
+
+            stock.Add("Bier", 10);
+            stock.Add("Wasser", 10);
+        }
+
+        public void PrintStock()
+        {
+            foreach (var item in stock)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void PrintPrices()
+        {
+            foreach (var item in prices)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public static void Main()
+    {
+
+        VendingMachine THN = new VendingMachine();
+        THN.PrintStock();
+    }
+}
